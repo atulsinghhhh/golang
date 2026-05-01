214823a8cffb467c4f0f5ebe38f8869a93d89edb
atulsinghhhh <atulsinghhh25@gmail.com>
2026-05-01 23:33:26 +0530

    run workers in fixed limit

--- concurrency/main.go
+++ concurrency/main.go
@@ -6,16 +6,30 @@ import (
 	"time"
 )
 
-func worker(url string,wg *sync.WaitGroup,resultChan chan Result) {
+
+
+func worker(jobsChan chan string,wg *sync.WaitGroup,resultChan chan Result) {
 	defer wg.Done()  // this used for cleaning function
-	time.Sleep(time.Millisecond*50)
 
-	fmt.Printf("image processed: %s\n",url)
+	for result := range jobsChan { // read
+		time.Sleep(time.Millisecond*50)
+		fmt.Printf("image processed: %s\n",result)
+		// fmt.Printf("recived: %v\n",result);
 
-	resultChan <- Result{
-		value: url,
-		err: nil,
-	} // write
+		resultChan <- Result{
+			value: result,
+			err: nil,
+		} //
+		
+	}
+	
+
+	fmt.Println("Worker shutting down");
+
+	// resultChan <- Result{
+	// 	value: url,
+	// 	err: nil,
+	// } // write
 	// wg.Done()
 }
 
@@ -25,20 +39,65 @@ type Result struct {
 }
 
 func main() {
+
+	jobs := []string{
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+		"image_1.png",
+	}
 	var wg sync.WaitGroup
 
-	resultChan:=make(chan Result, 2)
+	// total not workers
+	totalWorkers:= 7
+
+	resultChan:=make(chan Result, 50)
+	jobsChan:= make(chan string,len(jobs))
+
+	// keeps listening for jobs until the channel is closed
+	for i:=1;i<=totalWorkers;i++{
+		wg.Add(1)
+		go worker(jobsChan,&wg,resultChan)
+	}
+
 
 	startTime:= time.Now()
-	wg.Add(2)
-	go worker("image_.png",&wg, resultChan)
-	go worker("image2_.png",&wg,resultChan)
+	// wg.Add(2)
 
-	wg.Wait()
-	close(resultChan)
+	// for _,job :=range jobs {
+	// }
+	// go worker("image_.png",&wg, resultChan)
+	// go worker("image2_.png",&wg,resultChan)
+
+	go func () {
+		wg.Wait()
+		close(resultChan)
+
+	} ()
+
+	// send the jobs
+	for i:=0;i<len(jobs);i++{
+		jobsChan <-jobs[i]
+	}
+
+	close(jobsChan)
 
 	for result := range resultChan { // read
-		fmt.Printf("recived: %v\n",result);
+		fmt.Printf("jobs completed: %v\n",result);
 		if result.err !=nil {
 			// handle queue
 		}
