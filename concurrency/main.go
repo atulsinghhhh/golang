ed695127904cc279b71e55b5f68b803eec64cfdd
atulsinghhhh <atulsinghhh25@gmail.com>
2026-05-01 23:04:03 +0530

    learn fan in and fan out

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,23 +2,49 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func worker(url string) string{
+func worker(url string,wg *sync.WaitGroup,resultChan chan Result) {
+	defer wg.Done()  // this used for cleaning function
 	time.Sleep(time.Millisecond*50)
 
 	fmt.Printf("image processed: %s\n",url)
-	return url
+
+	resultChan <- Result{
+		value: url,
+		err: nil,
+	} // write
+	// wg.Done()
+}
+
+type Result struct {
+	value string
+	err error
 }
 
 func main() {
-	startTime:= time.Now()
-	result1:=worker("image_.png")
-	result2:=worker("image2_.png")
+	var wg sync.WaitGroup
 
-	fmt.Println("result",result1)
-	fmt.Println("result",result2)
+	resultChan:=make(chan Result, 2)
+
+	startTime:= time.Now()
+	wg.Add(2)
+	go worker("image_.png",&wg, resultChan)
+	go worker("image2_.png",&wg,resultChan)
+
+	wg.Wait()
+	close(resultChan)
+
+	for result := range resultChan { // read
+		fmt.Printf("recived: %v\n",result);
+		if result.err !=nil {
+			// handle queue
+		}
+	}
+	// fmt.Println("result",&wg)
+	// fmt.Println("result",&wg)
 	fmt.Printf("it took %s ms",time.Since(startTime))
 	
 }
